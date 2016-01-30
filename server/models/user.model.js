11a7e751476499500cda5ebbf377a584c0de6d3b
Valerii Iatsko <valerii.iatsko@icloud.com>
2016-01-30 23:21:12 +0000

    Reorganizaed server stuff a bit

---
+++ server/models/user.model.js
@@ -0,0 +1,205 @@
+import mongoose from 'mongoose';
+import { Schema } from 'mongoose';
+import crypto from 'crypto';
+import validator from 'validator';
+import generatePassword from 'generate-password';
+import owasp from 'owasp-password-strength-test';
+
+/**
+ * A Validation function for local strategy properties
+ */
+const validateLocalStrategyProperty = function validateLocalStrategyProperty(property) {
+  return ((this.provider !== 'local' && !this.updated) || property.length);
+};
+
+/**
+ * A Validation function for local strategy email
+ */
+const validateLocalStrategyEmail = function validateLocalStrategyEmail(email) {
+  return ((this.provider !== 'local' && !this.updated) ||
+          validator.isEmail(email, { require_tld: false }));
+};
+
+/**
+ * User Schema
+ */
+const UserSchema = new Schema({
+  firstName: {
+    type: String,
+    trim: true,
+    default: '',
+    validate: [validateLocalStrategyProperty, 'Please fill in your first name'],
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    default: '',
+    validate: [validateLocalStrategyProperty, 'Please fill in your last name'],
+  },
+  displayName: {
+    type: String,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    default: '',
+    validate: [validateLocalStrategyEmail, 'Please fill a valid email address'],
+  },
+  username: {
+    type: String,
+    unique: 'Username already exists',
+    required: 'Please fill in a username',
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    default: '',
+  },
+  salt: {
+    type: String,
+  },
+  profileImageURL: {
+    type: String,
+    default: 'modules/users/client/img/profile/default.png',
+  },
+  provider: {
+    type: String,
+    required: 'Provider is required',
+  },
+  providerData: {},
+  additionalProvidersData: {},
+  roles: {
+    type: [{
+      type: String,
+      enum: ['user', 'admin'],
+    }],
+    default: ['user'],
+    required: 'Please provide at least one role',
+  },
+  updated: {
+    type: Date,
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+  /* For reset password */
+  resetPasswordToken: {
+    type: String,
+  },
+  resetPasswordExpires: {
+    type: Date,
+  },
+});
+
+/**
+ * Hook a pre save method to hash the password
+ */
+UserSchema.pre('save', function preSave(next) {
+  if (this.password && this.isModified('password')) {
+    this.salt = crypto.randomBytes(16).toString('base64');
+    this.password = this.hashPassword(this.password);
+  }
+
+  next();
+});
+
+/**
+ * Hook a pre validate method to test the local password
+ */
+UserSchema.pre('validate', function preValidate(next) {
+  if (this.provider === 'local' && this.password && this.isModified('password')) {
+    const result = owasp.test(this.password);
+    if (result.errors.length) {
+      const error = result.errors.join(' ');
+      this.invalidate('password', error);
+    }
+  }
+
+  next();
+});
+
+/**
+ * Create instance method for hashing a password
+ */
+UserSchema.methods.hashPassword = function hashPassword(password) {
+  if (this.salt && password) {
+    return crypto.pbkdf2Sync(password, new Buffer(this.salt, 'base64'), 10000, 64)
+                 .toString('base64');
+  }
+
+  return password;
+};
+
+/**
+ * Create instance method for authenticating user
+ */
+UserSchema.methods.authenticate = function authenticate(password) {
+  return this.password === this.hashPassword(password);
+};
+
+/**
+ * Find possible not used username
+ */
+UserSchema.statics.findUniqueUsername = function findUniqueUsername(username, suffix, callback) {
+  const possibleUsername = username.toLowerCase() + (suffix || '');
+
+  this.findOne({
+    username: possibleUsername,
+  }, (err, user) => {
+    if (!err) {
+      if (!user) {
+        callback(possibleUsername);
+      } else {
+        return this.findUniqueUsername(username, (suffix || 0) + 1, callback);
+      }
+    } else {
+      callback(null);
+    }
+  });
+};
+
+/**
+* Generates a random passphrase that passes the owasp test
+* Returns a promise that resolves with the generated passphrase,
+*  or rejects with an error if something goes wrong.
+* NOTE: Passphrases are only tested against the required owasp strength tests,
+*  and not the optional tests.
+*/
+UserSchema.statics.generateRandomPassphrase = function generateRandomPassphrase() {
+  return new Promise((resolve, reject) => {
+    let password = '';
+    const repeatingCharacters = new RegExp('(.)\\1{2,}', 'g');
+
+    // iterate until the we have a valid passphrase
+    // NOTE: Should rarely iterate more than once,
+    //  but we need this to ensure no repeating characters are present
+    while (password.length < 20 || repeatingCharacters.test(password)) {
+      // build the random password
+      password = generatePassword.generate({
+        length: Math.floor(Math.random() * (20)) + 20, // randomize between 20 and 40 characters
+        numbers: true,
+        symbols: false,
+        uppercase: true,
+        excludeSimilarCharacters: true,
+      });
+
+      // check if we need to remove any repeating characters
+      password = password.replace(repeatingCharacters, '');
+    }
+
+    // Send the rejection back if the passphrase fails to pass the strength test
+    if (owasp.test(password).errors.length) {
+      reject(new Error('An unexpected problem occured while generating the random passphrase'));
+    } else {
+      // resolve with the validated passphrase
+      resolve(password);
+    }
+  });
+};
+
+mongoose.model('User', UserSchema);
